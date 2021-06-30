cd9e811909980dd7456248fd66c535cecc25285b
AdvaOren1 <AdvaOren@carmel6000.amitnet.org>
2021-06-30 23:25:09 +0300

    vim & nano exercise

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CountBags from './components/CountBags';
+
+function App() {
+  return (
+    <div className="App">
+      <CountBags />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CountBags.jsx
@@ -0,0 +1,50 @@
+import { useRef, useState } from 'react'
+
+
+function CountBags() {
+    const [counter, setCounter] = useState(0);
+    const [newValue, setNewValue] = useState('');
+    const [textArr, setTextArr] = useState([])
+    const inputElement = useRef(null);
+
+    const addBag = () => {
+        console.log("be-tap2tel yesh harbe caele")
+        setCounter(1);
+    };
+    const addText = () => {
+        console.log("be-tap2tel ze yoter tov");
+        let newArr = [...textArr]
+        newArr.push(newValue)
+        console.log('newArr: ', newArr);
+        setTextArr(newArr);
+        setNewValue('')
+    }
+    
+    const handleChange = (event) => {
+        stamFunction()
+        setNewValue(event.target.value);
+    }
+
+    const stamFunction =()=>{
+        console.log("en al tap2tel");
+    }
+
+    return (
+        <div className="App">
+            <h1>Count Your Bags!</h1>
+            <button onClick={addBag}>Add Bags!</button>
+            <button>Remove Bags!</button>
+            <h4>bags counter: {counter}</h4>
+            <h1>Not talking about the bugs:</h1>
+            <input ref={inputElement}
+                id='name' type="text" name="name" placeholder="שם" autoComplete='off' value={newValue} onChange={(e) => handleChange(e)}></input>
+
+            <button onClick={addText}>Add</button>
+            {textArr.map((text) => {
+                return <li>{text}</li>
+            })}
+        </div>
+    );
+}
+
+export default CountBags;
